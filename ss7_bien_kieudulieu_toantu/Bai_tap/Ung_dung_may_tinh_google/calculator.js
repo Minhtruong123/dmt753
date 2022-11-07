a1472c0c31cb828d4debc2766f6ae94bc1558cf5
Minhtruong123 <duongminhtruong1234@gmail.com>
2022-11-07 20:18:29 +0700

    update

--- ss7_bien_kieudulieu_toantu/Bai_tap/Ung_dung_may_tinh_google/calculator.js
+++ ss7_bien_kieudulieu_toantu/Bai_tap/Ung_dung_may_tinh_google/calculator.js
@@ -2,11 +2,10 @@ var result='';
 var a;
 var c;
 var b;
+var ketqua;
+var slice2;
+var slice1;
 function calculate(operate) {
-    let result2;
-    let ketqua;
-    let slice1;
-    let slice2;
     let dau;
     if (operate == '0'){
         result += operate;
@@ -50,6 +49,8 @@ function calculate(operate) {
     }
     if (operate == 'AC'){
         result ='';
+        ketqua=0;
+        a=0;
         document.getElementById('result').innerHTML=result;
     }
     if (operate == '.'){
@@ -84,48 +85,47 @@ function calculate(operate) {
         result += operate;
         document.getElementById('result').innerHTML=result;
     }
-
-    //kết quả
-    //Ý tưởng: Tìm dấu trong chuỗi string rồi tách chuỗi, xong bắt đầu tính
+//     kết quả
+//     Ý tưởng: Tìm dấu trong chuỗi string rồi tách chuỗi, xong bắt đầu tính
     if (operate == '='){
         dau=result.indexOf('+');
         if (dau >> -1) {
-            result2=result;
-            slice1= parseInt(result2.slice(0,dau-1));
-            slice2= parseInt(result2.slice(1,result2.length));
+            slice1=parseInt(result.slice(0,dau));
+            slice2= parseInt(result.slice(dau+1,result.length));
             ketqua= slice1+slice2;
             dau=-1;
         }
-        dau=result.indexOf('-');
-        if (dau >> -1) {
-            result2=result;
-            slice1= parseInt(result2.slice(0,dau-1));
-            slice2= parseInt(result2.slice(dau+1,result2.length));
-            ketqua= slice1-slice2;
-            dau=-1;
-        }
-        dau=result.indexOf('*');
-        if (dau >> -1) {
-            result2=result;
-            slice1= parseInt(result2.slice(0,dau-1));
-            slice2= parseInt(result2.slice(dau+1,result2.length));
-            ketqua= slice1*slice2;
-            dau=-1;
-        }
-        dau=result.indexOf('/');
-        if (dau >> -1) {
-            result2=result;
-            slice1= parseInt(result2.slice(0,dau-1));
-            slice2= parseInt(result2.slice(dau+1,result2.length));
-            ketqua= slice1/slice2;
-            dau=-1;
-        }
-        // dau=result.indexOf('%');
+        alert(slice2)
+        // dau=result.indexOf('-');
         // if (dau >> -1) {
-        //     slice1= parseInt(result.slice(0,dau-1));
-        //     ketqua= slice1/100;
+        //     result2=result;
+        //     slice1= parseInt(result2.slice(0,dau));
+        //     slice2= parseInt(result2.slice(dau+1,result2.length));
+        //     ketqua= slice1-slice2;
         //     dau=-1;
         // }
-        document.getElementById('result').innerHTML=ketqua;
-    }
+        // dau=result.indexOf('*');
+        // if (dau >> -1) {
+        //     result2=result;
+        //     slice1= parseInt(result2.slice(0,dau));
+        //     slice2= parseInt(result2.slice(dau+1,result2.length));
+        //     ketqua= slice1*slice2;
+        //     dau=-1;
+        // }
+        // dau=result.indexOf('/');
+        // if (dau >> -1) {
+        //     result2=result;
+        //     slice1= parseInt(result2.slice(0,dau));
+        //     slice2= parseInt(result2.slice(dau+1,result2.length));
+        //     ketqua= slice1/slice2;
+        //     dau=-1;
+        // }
+//         // dau=result.indexOf('%');
+//         // if (dau >> -1) {
+//         //     slice1= parseInt(result.slice(0,dau-1));
+//         //     ketqua= slice1/100;
+//         //     dau=-1;
+//         // }
+//          document.getElementById('result').innerHTML=ketqua;
+     }
 }
