c9cb9813967e3dc9aa61ecb0e5fdeefdcf6de420
Minhtruong123 <duongminhtruong1234@gmail.com>
2022-11-07 15:10:12 +0700

    update

--- ss7_bien_kieudulieu_toantu/Bai_tap/Ung_dung_may_tinh/ung_dung_may_tinh.js
+++ ss7_bien_kieudulieu_toantu/Bai_tap/Ung_dung_may_tinh/ung_dung_may_tinh.js
@@ -1,24 +1,10 @@
-function add() {
+function calculate(operate) {
     var first= parseInt(document.getElementById('a').value);
     var second=parseInt(document.getElementById('b').value);
-    var r=first+second
-    document.getElementById('result').innerHTML='Result:'+ r;
-}
-function sub() {
-    var first= parseInt(document.getElementById('a').value);
-    var second=parseInt(document.getElementById('b').value);
-    var r=first-second
-    document.getElementById('result').innerHTML='Result:'+ r;
-}
-function mul() {
-    var first= parseInt(document.getElementById('a').value);
-    var second=parseInt(document.getElementById('b').value);
-    var r=first*second
-    document.getElementById('result').innerHTML='Result:'+ r;
-}
-function div() {
-    var first= parseInt(document.getElementById('a').value);
-    var second=parseInt(document.getElementById('b').value);
-    var r=first/second
+    var r;
+    if (operate== '+') r = first + second
+        else if (operate== '-') r = first - second;
+        else if (operate== '*') r= first * second;
+        else if (operate== '/') r= first / second;
     document.getElementById('result').innerHTML='Result:'+ r;
 }
